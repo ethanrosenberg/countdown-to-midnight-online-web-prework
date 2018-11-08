11976dc09f80ee77a37a6f9d39f82642cea21d4f
Ethan Rosenberg <ethan@ethanrosenberg.com>
2018-11-08 09:50:23 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -2,11 +2,11 @@
 
 def countdown(number)
   
-  counter = 0
+  counter = number
   
-  while counter < number
+  while counter > 0
     puts "#{number} SECOND(S)!"
-    counter += 1
+    counter -= 1
   end
 
   puts "HAPPY NEW YEAR!"
